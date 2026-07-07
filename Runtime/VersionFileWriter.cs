18e8955928718819839fa2b23cf08e860ff14ebf
QB <151462845+QuestionBlock64@users.noreply.github.com>
2026-07-07 16:06:09 +0100

    Add files via upload

--- Runtime/VersionFileWriter.cs
+++ Runtime/VersionFileWriter.cs
@@ -23,7 +23,7 @@ namespace MiSideMultiplayer
 
             string versionFile = Path.Combine(dataDirectory, "Version.txt");
             string text =
-                "MiSide Multiplayer DEV Test" + Environment.NewLine +
+                "MiSide Multiplayer Pre Release" + Environment.NewLine +
                 pluginName + Environment.NewLine +
                 "Version: " + pluginVersion + Environment.NewLine +
                 "Runtime: BepInEx 6 IL2CPP" + Environment.NewLine +
